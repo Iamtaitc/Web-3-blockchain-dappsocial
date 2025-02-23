08215d41bfa790195166674ea0664f263ab4071a
hoainam <phanhoainam.work@gmail.com>
2025-02-23 19:13:02 +0700

    Navbar profile

---
+++ FE/src/Components/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { FaHome, FaSearch, FaLeaf, FaPlus, FaCrown, FaQuestion, FaWallet, FaBars } from "react-icons/fa";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/17fff740-d526-4b77-a49f-093d9ac45b2e/dj8ebp0-271148db-4795-4189-bdd0-d8bdb4320bbd.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzE3ZmZmNzQwLWQ1MjYtNGI3Ny1hNDlmLTA5M2Q5YWM0NWIyZVwvZGo4ZWJwMC0yNzExNDhkYi00Nzk1LTQxODktYmRkMC1kOGJkYjQzMjBiYmQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0._nIe5Pg1XwHlGtpTzgptwlgPVcAr5i0ZABGlubB9inQ" alt="" />
+      </div>
+      <ul className="nav-links">
+        <li><Link to="/"><FaHome /><span>Home</span></Link></li>
+        <li><Link to="/find-nft"><FaSearch /><span>Find NFT</span></Link></li>
+        <li><Link to="/farm"><FaLeaf /><span>Farm</span></Link></li>
+        <li><Link to="/add-nft"><FaPlus /><span>Add NFT</span></Link></li>
+        <li><Link to="/premium"><FaCrown /><span>Premium</span></Link></li>
+        <li><Link to="/quest"><FaQuestion /><span>Quest</span></Link></li>
+        <li><Link to="/wallet"><FaWallet /><span>Wallet</span></Link></li>
+      </ul>
+      <div className="menu">
+        <FaBars />
+        <span>Menu</span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ FE/src/pages/ProfileNFT.tsx
@@ -0,0 +1,53 @@
+// pages/Profile.tsx
+import React from "react";
+import NFTCard from "../Components/NFTCard";
+import "./ProfileNFT.css"; // Import CSS riêng cho Profile
+
+interface NFTItem {
+  image: string;
+  title: string;
+  price: string;
+}
+
+const ProfileNFT: React.FC = () => {
+  const nfts: NFTItem[] = [
+    { image: "/nft1.jpg", title: "NFT Name", price: "0.39341" },
+    { image: "/nft2.jpg", title: "NFT Name", price: "0.39341" },
+    { image: "/nft3.jpg", title: "NFT Name", price: "0.39341" },
+    { image: "/nft4.jpg", title: "NFT Name", price: "0.39341" },
+    { image: "/nft2.jpg", title: "NFT Name", price: "0.39341" },
+    { image: "/nft3.jpg", title: "NFT Name", price: "0.39341" },
+    { image: "/nft4.jpg", title: "NFT Name", price: "0.39341" },
+    { image: "/nft2.jpg", title: "NFT Name", price: "0.39341" },
+  ];
+
+  return (
+    <div className="profile-container">
+      {/* Profile Header */}
+      <div className="profile-header">
+        <img src="https://a.deviantart.net/avatars-big/s/p/spiraloso.png?9" alt="Logo" className="profile-logo" />
+        <div>
+          <h1 className="profile-name">Nin0912</h1>
+          <p className="profile-stats">
+            Followers: 14k | Favorites: 982 | Products: 82 | Sold: 102
+          </p>
+          <p className="profile-description">
+            Web-3-blockchain-dappsocial is an advanced decentralized
+            application (DApp) platform that allows users to easily create,
+            list, and trade NFTs securely.
+          </p>
+        </div>
+      </div>
+
+      {/* NFT Card List */}
+      <div className="nft-list">
+        {nfts.map((nft, index) => (
+          <NFTCard key={index} image={nft.image} title={nft.title} price={nft.price} />
+        ))}
+      </div>
+      
+    </div>
+  );
+};
+
+export default ProfileNFT;
