8d2bb5ea2d1edcfa7aae37a7cb2bf70816638f2b
truong cong bao <bao077559216@gmail.com>
2025-02-23 19:27:01 +0700

    lưu khi pull

---
+++ FE/src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+      <header>
+        <h1>Thanh menu</h1>
+      </header>
+    );
+  };
+  
+  export default Header;
---
+++ FE/src/pages/home.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <div className="bg-black">
+      <Header />
+      <p>This is the Home page</p>
+    </div>
+  );
+};
+
+export default Home;
+
